b64740d9426204f4e19f5be61773fbabb9315f45
nurhamsah <nur.hamsah.cash@gmail.com>
2021-12-15 16:02:29 +0700

    Fix error

--- src/Header.js
+++ src/Header.js
@@ -28,7 +28,7 @@ function Header() {
         sdfiadgsfoakfnkjasdbfnsdjkf
       </p>
       <button className="text-teal-100 border border-teal-300 px-5 py-3 font-bold rounded ease-in duration-300 hover:bg-teal-300 shadow-lg hover:shadow-cyan-500/50 hover:text-black">
-        masok
+        Masuk
       </button>
     </div>
   );
