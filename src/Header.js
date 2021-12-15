da4c063475146776c6e25dc9dd5639e6a46bb3c3
nurhamsah <nur.hamsah.cash@gmail.com>
2021-12-15 15:41:58 +0700

    commit pertama

---
+++ src/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="bg-slate-900">
+      <p className="text-white">Hallo, Perkenalkan Namaku</p>
+      <p
+        className="text-white font-bold"
+        style={{
+          fontSize: '80px',
+        }}
+      >
+        NURHAMSAH.
+      </p>
+      <p
+        className="text-white"
+        className="text-white font-bold"
+        style={{
+          fontSize: '80px',
+        }}
+      >
+        i Build Things For The Web.
+      </p>
+      <p className="text-white">
+        dfhsd fsldkjvnskdhsikdnskdlgn sdlkfnsdfkjsnbdlfkjn sdlkfnsdfkjsnbdlfkjn
+        ksdjfnisdkfj ssdjfnskdjbfakdljfod skdjnfskd flsdksjdnflkasdfksdj,f asdf
+        ksdjfnakdflka.jfkjdnfsladkfjwighwuhrejsndfjsdoifhjwensdlk
+        sdfiadgsfoakfnkjasdbfnsdjkf
+      </p>
+      <button className="text-teal-100 border border-teal-300 px-5 py-3 font-bold rounded ease-in duration-300 hover:bg-teal-300 shadow-lg hover:shadow-cyan-500/50 hover:text-black">
+        masok
+      </button>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="flex justify-between p-5 items-center bg-slate-900">
+      <div className="">
+        <span className="font-bold tracking-wide text-white text-2xl duration-200 ">
+          NUR
+          <span className="text-teal-500 ">HAMSAH</span>
+        </span>
+      </div>
+
+      <div className="flex items-center">
+        <ul className="hidden lg:flex md:grid">
+          <li>
+            <a className="font-mono text-teal-100 mr-10" href="">
+              Tentang
+            </a>
+          </li>
+          <li>
+            <a className="font-mono text-teal-100 mr-10" href="">
+              Pengalaman
+            </a>
+          </li>
+          <li>
+            <a className="font-mono text-teal-100 mr-10" href="">
+              Pekerjaan
+            </a>
+          </li>
+          <li>
+            <a className="font-mono text-teal-100 mr-10" href="">
+              Contact
+            </a>
+          </li>
+        </ul>
+
+        <button className="text-teal-100 border border-teal-300 px-5 py-3 font-bold rounded ease-in duration-300 hover:bg-teal-300 shadow-lg hover:shadow-cyan-500/50 hover:text-black">
+          resume
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
