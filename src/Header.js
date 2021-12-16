d6d5ff9f57d8426034b95ef334ed16132372cacb
nurhamsah <nur.hamsah.cash@gmail.com>
2021-12-16 09:33:26 +0700

    Fix style

--- src/Header.js
+++ src/Header.js
@@ -1,32 +1,27 @@
-import React from "react";
+import React from 'react';
 
 function Header() {
   return (
     <div className="bg-slate-900 p-20">
       <p className="text-white font-mono">Hallo, Perkenalkan Namaku</p>
-      <p
-        className="text-white font-bold"
-        style={{
-          fontSize: "80px",
-        }}
-      >
-        NURHAMSAH.
-      </p>
-      <p
-        className="text-white"
-        className="text-white font-bold"
-        style={{
-          fontSize: "80px",
-        }}
-      >
-        i Build Things For The Web.
+      <p className="text-white font-bold text-5xl">NURHAMSAH.</p>
+      <p className="text-white font-bold text-8xl mb-5">
+        i Build Things For <span className="text-teal-500 ">The Web.</span>
       </p>
       <p className="text-white max-w-3xl font-mono">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa debitis aliquam, perferendis porro magnam officiis id numquam quidem laborum nobis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa debitis aliquam, perferendis
-        porro magnam officiis id numquam quidem laborum nobis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa debitis
+        aliquam, perferendis porro magnam officiis id numquam quidem laborum
+        nobis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+        debitis aliquam, perferendis porro magnam officiis id numquam quidem
+        laborum nobis.
       </p>
       <button className="text-teal-100 border border-teal-300 justify-center p-3 mt-10 font-bold rounded ease-in duration-300 hover:bg-teal-300 shadow-lg hover:shadow-cyan-500/50 hover:text-black flex">
-        Get in Touch <img className="ml-3" src="https://www.svgrepo.com/show/354560/whatsapp.svg" width="7%" />
+        Get in Touch{' '}
+        <img
+          className="ml-3"
+          src="https://www.svgrepo.com/show/354560/whatsapp.svg"
+          width="7%"
+        />
       </button>
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 function Navbar() {
   return (
@@ -13,28 +13,42 @@ function Navbar() {
       <div className="flex items-center">
         <ul className="hidden lg:flex md:grid mr-10">
           <li>
-            <a className="hover:bg-teal-300 p-2 duration-300 hover:text-black font-mono text-teal-100 mr-10" href="">
+            <a
+              className="hover:bg-teal-300 p-2 duration-300 hover:text-black font-mono text-teal-100 mr-10"
+              href=""
+            >
               Tentang
             </a>
           </li>
           <li>
-            <a className="hover:bg-teal-300 p-2 duration-300 hover:text-black font-mono text-teal-100 mr-10" href="">
+            <a
+              className="hover:bg-teal-300 p-2 duration-300 hover:text-black font-mono text-teal-100 mr-10"
+              href=""
+            >
               Pengalaman
             </a>
           </li>
           <li>
-            <a className="hover:bg-teal-300 p-2 duration-300 hover:text-black font-mono text-teal-100 mr-10" href="">
+            <a
+              className="hover:bg-teal-300 p-2 duration-300 hover:text-black font-mono text-teal-100 mr-10"
+              href=""
+            >
               Pekerjaan
             </a>
           </li>
           <li>
-            <a className="hover:bg-teal-300 p-2 duration-300 hover:text-black font-mono text-teal-100 mr-100" href="">
+            <a
+              className="hover:bg-teal-300 p-2 duration-300 hover:text-black font-mono text-teal-100 mr-100"
+              href=""
+            >
               Contact
             </a>
           </li>
         </ul>
 
-        <button className="text-teal-100 border border-teal-300 px-5 py-3 font-bold rounded ease-in duration-300 hover:bg-teal-300 shadow-lg hover:shadow-cyan-500/50 hover:text-black">resume</button>
+        <button className="text-teal-100 border border-teal-300 px-5 py-3 font-bold rounded ease-in duration-300 hover:bg-teal-300 shadow-lg hover:shadow-cyan-500/50 hover:text-black">
+          resume
+        </button>
       </div>
     </div>
   );
