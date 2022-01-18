fd8f35595d280ef8b1965692a7b8434484934207
nurhamsah <nur.hamsah.cash@gmail.com>
2022-01-18 14:49:35 +0700

    done

--- src/Navbar.js
+++ src/Navbar.js
@@ -41,7 +41,7 @@ function Navbar() {
             </a>
           </li>
         </ul>
-        <a href="https://brittanychiang.com/">
+        <a href="https://wa.me/6281213221343">
           <button className="text-teal-100 border border-teal-300 px-5 py-3 font-bold rounded ease-in duration-300 hover:bg-teal-300 shadow-lg hover:shadow-cyan-500/50 hover:text-black">
             Hire Me
           </button>
